67c07b470a39023b6249666f0be5ea9369476862
HaiderABB <haider.a.moazzam@gmail.com>
2024-08-28 19:28:27 +0500

    Bug Fix

--- application/routers/auth.py
+++ application/routers/auth.py
@@ -4,6 +4,7 @@ from schemas.user import user_registration, user_registration_response
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from models.users import users
+from sqlalchemy import text
 
 
 auth_router = APIRouter(prefix="/FreshFinds/auth")
@@ -11,18 +12,24 @@ auth_router = APIRouter(prefix="/FreshFinds/auth")
 
 def user_registeration(user_data, db: Session):
 
-    result = db.query(
-        func.register_user(
-            user_data.username, user_data.email, user_data.password).label('user')
-    )
+    sql_command = text("""
+        SELECT register_user(:username, :email, :password);
+    """)
 
-    return result
+    db.execute(sql_command, {
+        'username': user_data.username,
+        'email': user_data.email,
+        'password': user_data.password_hash
+    })
+
+    # Commit the transaction to ensure the changes are saved
+    db.commit()
 
 
 @auth_router.post("/register", status_code=200)
-async def register_user(payload: user_registration, db: Session = Depends(get_db)):
+def register_user(payload: user_registration, db: Session = Depends(get_db)):
 
     user_data = users(username=payload.username,
-                      email=payload.email, password=payload.password)
-    query_result = await user_registeration(user_data, db)
+                      email=payload.email, password_hash=payload.password)
+    query_result = user_registeration(user_data, db)
     return {"Message": "User Registered Successfully"}
